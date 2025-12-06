45cfba514c265166317932785ac21c03aef9990d
marinruin2221 <marinruin@naver.com>
2025-12-06 20:05:15 +0900

    불필요한 파일 삭제 font style 추가

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,8 +2,6 @@ import Client from "react-dom/client";
 
 import App from "@src/App.jsx";
 
-import "@css/tailwind.css";
-
 const body = document.querySelector("body");
 const root = Client.createRoot(body);
 
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { useEffect } from "react";
 
 import Header from "@layout/Header.jsx";
 import Footer from "@layout/Footer.jsx";
 
 export default function MainPage()
 {
-	useEffect(() => {
-		fetch("/api/message")
-		.then((res) => res.text())
-		.then((data) => console.log(data))
-	}, []);
-
 	return <React.Fragment>
 		<Header/>
-		mainpage
+
+		<form>
+			
+		</form>
+
 		<Footer/>
 	</React.Fragment>
-}
+} 
