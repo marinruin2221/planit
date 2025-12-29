ca77bda6c9ddcf592a0c15e39264f9c1d1269438
marinruin <marinruin@naver.com>
2025-12-29 21:05:04 +0900

    1. 메인페이지 이벤트 폼 연동

--- src/main/java/cteam/planit/main/controller/MainController.java
+++ src/main/java/cteam/planit/main/controller/MainController.java
@@ -1,15 +1,30 @@
 package cteam.planit.main.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cteam.planit.main.dao.Event;
+import cteam.planit.main.services.MainService;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
+@RequestMapping("/api/main")
 public class MainController
 {
-	@GetMapping("/api/message")
-	public String message() throws Exception
+	@Autowired
+	public MainService service;
+
+	@GetMapping("/eventSelect")
+	public List<Event> eventSelect() throws Exception
 	{
-		return "Hello Main Page";
+		return service.eventSelect();
 	}
 }
-
