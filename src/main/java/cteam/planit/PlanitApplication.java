d514d92b37c5265657a752f18256350216549c6e
marinruin2221 <marinruin@naver.com>
2026-01-03 22:06:32 +0900

    1. 더미 데이터 삭제

--- src/main/java/cteam/planit/PlanitApplication.java
+++ src/main/java/cteam/planit/PlanitApplication.java
@@ -1,7 +1,6 @@
 package cteam.planit;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
