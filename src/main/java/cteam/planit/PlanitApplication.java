5dd6d5ebd04ef2368026d2caba9a2574d348b92d
marinruin <marinruin@naver.com>
2026-01-06 18:36:50 +0900

    1. 마이페이지 리뷰 태스트 데이터 삭제

--- src/main/java/cteam/planit/PlanitApplication.java
+++ src/main/java/cteam/planit/PlanitApplication.java
@@ -1,7 +1,5 @@
 package cteam.planit;
 
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,17 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import cteam.planit.main.dao.BreakdownDAO;
 import cteam.planit.main.dao.BreakdownRepository;
-import cteam.planit.main.entity.Review;
-import cteam.planit.main.repository.ReviewRepository;
 
 @SpringBootApplication
 public class PlanitApplication implements CommandLineRunner
 {
 	@Autowired
 	private BreakdownRepository breakdownRepository;
-
-	@Autowired
-	private ReviewRepository reviewRepository;
 
 	public static void main(String[] args)
 	{
@@ -42,22 +35,6 @@ public class PlanitApplication implements CommandLineRunner
 			breakdownDAO.setStatus("3");
 			breakdownDAO.setDeleteYN("N");
 			breakdownRepository.save(breakdownDAO);
-		}
-
-		if(reviewRepository.count() > 5) return;
-		for(int i = 1; i <= 6; i++)
-		{
-			Review review = new Review();
-			review.setContentId("1");
-			review.setUserId("marinruin");
-			review.setName("서울 강남 호텔" + i);
-			review.setReviewerName("여행러버" + i);
-			review.setReviewerLevel(17);
-			review.setStars(3);
-			review.setContent("나쁘지 않았지만 기대만큼은 아니었어요" + i);
-			review.setReviewDate(LocalDate.of(2026, 01, 01));
-			review.setDeleteYN("N");
-			reviewRepository.save(review);
 		}
 	}
 }
