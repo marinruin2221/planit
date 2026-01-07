82125ad6ad6c552fd09e6684524d036d104c7119
marinruin <marinruin@naver.com>
2026-01-07 19:54:40 +0900

    1. 결제 후 예약 내역 연동

--- src/main/java/cteam/planit/PlanitApplication.java
+++ src/main/java/cteam/planit/PlanitApplication.java
@@ -1,19 +1,12 @@
 package cteam.planit;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import cteam.planit.main.dao.BreakdownDAO;
-import cteam.planit.main.dao.BreakdownRepository;
-
 @SpringBootApplication
 public class PlanitApplication implements CommandLineRunner
 {
-	@Autowired
-	private BreakdownRepository breakdownRepository;
-
 	public static void main(String[] args)
 	{
 		SpringApplication.run(PlanitApplication.class, args);
@@ -22,19 +15,6 @@ public class PlanitApplication implements CommandLineRunner
 	@Override
 	public void run(String... args) throws Exception
 	{
-		if(breakdownRepository.count() > 5) return;
-		for(int i = 1; i <= 6; i++)
-		{
-			BreakdownDAO breakdownDAO = new BreakdownDAO();
-			breakdownDAO.setUserId("marinruin");
-			breakdownDAO.setContentId("1");
-			breakdownDAO.setName("서울 강남 호텔" + i);
-			breakdownDAO.setDateF("2025-01-05");
-			breakdownDAO.setDateT("2026-01-07");
-			breakdownDAO.setPrice("150000");
-			breakdownDAO.setStatus("1");
-			breakdownDAO.setDeleteYN("N");
-			breakdownRepository.save(breakdownDAO);
-		}
+		
 	}
 }
